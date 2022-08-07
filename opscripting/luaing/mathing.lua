5aa640b6b6d1d1790dc5f050c9bbc219c1f4584d
Liam Collod <lcollod@gmail.com>
2022-08-07 18:57:52 +0200

    [luaing] removed lllogger import, formatted

--- opscripting/luaing/mathing.lua
+++ opscripting/luaing/mathing.lua
@@ -1,22 +1,26 @@
-local logging = require("lllogger")
-local logger = logging:get_logger("luaing.opsbase.mathing")
-
 local _M = {}
-_M["logger"] = logger
 
 -- we make some global functions local as this will improve performances in
 -- heavy loops.
 local mathpi = math.pi
 local mathfloor = math.floor
-
+local stringformat = string.format
 
 function _M.round(a, prec)
-    --[[
-    SRC: https://stackoverflow.com/questions/9654496/lua-converting-from-float-to-int
-    ]]
-    return mathfloor(a + 0.5*prec) -- where prec is 10^n, starting at 0
+  --[[
+  SRC: https://stackoverflow.com/questions/9654496/lua-converting-from-float-to-int
+  ]]
+  return mathfloor(a + 0.5 * prec) -- where prec is 10^n, starting at 0
 end
 
+function _M.round2(num, numDecimalPlaces)
+  --[[
+  Should be slower than round(). To verify.
+
+  SRC: http://lua-users.org/wiki/SimpleRound
+  ]]
+  return tonumber(stringformat("%." .. (numDecimalPlaces or 0) .. "f", num))
+end
 
 function _M.degree_to_radian(rotation)
   --[[
@@ -26,9 +30,8 @@ function _M.degree_to_radian(rotation)
     num:
       rotation value converted to radian
   ]]
-  return rotation * (mathpi/180.0)
+  return rotation * (mathpi / 180.0)
 end
-
 
 function _M.radian_to_degree(radian)
   --[[
@@ -38,8 +41,7 @@ function _M.radian_to_degree(radian)
     num:
       radian value converted to degree
   ]]
-  return radian * (180.0/mathpi)
+  return radian * (180.0 / mathpi)
 end
-
 
 return _M
