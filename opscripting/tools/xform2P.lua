34a1d76a1ebfe16df469116d3382b9f429e82a21
Liam Collod <lcollod@gmail.com>
2022-08-08 19:12:31 +0200

    [tools] added xform2p

---
+++ opscripting/tools/xform2P.lua
@@ -0,0 +1,75 @@
+--[[
+Merge xform transformations to the `geometry.point.P` attribute.
+
+! If your xform transform is interactive, think to disable
+this ops before trying to move it in the viewer.
+
+Supports motion blur.
+
+[OpScript]
+parameters.location = "location(s) to merge the xform attribute"
+parameters.applyWhere = "at specific location OR at locations matching CEL"
+]]
+local katlua = {}
+katlua.retrieve = require("opscripting.katlua.retrieve")
+local logging = require("lllogger")
+
+local logger = logging:get_logger("tools.xform2P")
+
+-- make a global local to improve perfs in big loops
+local v3d = Imath.V3d
+
+local function run()
+
+  logger:debug("[run] Started for location=", Interface.GetInputLocationPath())
+  local stime = os.clock()
+
+  local points_attr = katlua.retrieve.getAttr("geometry.point.P")
+
+  local xform = Interface.GetGlobalXFormGroup(Interface.GetInputLocationPath(), 0)
+  local matrix_attr = XFormUtils.CalcTransformMatrixAtExistingTimes(xform)  -- DoubleAttribute
+
+  local matrix
+  local points
+  local points_new = {}
+  local pvector
+  local pnew
+
+  for smplindex = 0, matrix_attr:getNumberOfTimeSamples() - 1 do
+    pnew = {}
+    smplindex = matrix_attr:getSampleTime(smplindex)
+    points = points_attr:getNearestSample(smplindex)
+    matrix = Imath.M44d(matrix_attr:getNearestSample(smplindex))
+
+    for i = 0, #points / 3 - 1 do
+
+      pvector = v3d(
+          points[i * 3 + 1],
+          points[i * 3 + 2],
+          points[i * 3 + 3]
+      ) * matrix
+
+      pnew[#pnew + 1] = pvector.x
+      pnew[#pnew + 1] = pvector.y
+      pnew[#pnew + 1] = pvector.z
+
+    end
+
+    points_new[smplindex] = pnew
+
+  end
+
+  Interface.SetAttr("geometry.point.P", FloatAttribute(points_new, 3))
+  Interface.DeleteAttr("xform")
+
+  logger:info(
+      "[run] Finished in ",
+      os.clock() - stime,
+      "s for location",
+      Interface.GetInputLocationPath()
+  )
+  return
+
+end
+
+return run
---
+++ opscripting/tools/xform2P.py
@@ -0,0 +1,53 @@
+import os.path
+
+from Katana import NodegraphAPI
+
+from opscripting.tooling import createDefaultCustomTool
+
+NAME = os.path.splitext(os.path.basename(__file__))[0]
+VERSION = "1.0.0"
+
+
+def configOpScript(node):
+    # type: (NodegraphAPI.Node) -> None
+    script = """
+local script = require("opscripting.tools.{NAME}")
+script()"""
+    script = script.format(NAME=NAME)
+
+    node.getParameter("CEL").setExpression("=^/user.CEL", True)
+    node.getParameter("applyWhere").setValue("at locations matching CEL", 0)
+    node.getParameter("script.lua").setValue(script, 0)
+    return
+
+
+def configTool(node):
+    # type: (NodegraphAPI.Node) -> None
+
+    userparam = node.getParameter("user")
+
+    p = userparam.createChildString("CEL", "")
+    hint = {"widget": "cel"}
+    p.setHintString(repr(hint))
+
+    return
+
+
+def build():
+    # type: () -> NodegraphAPI.Node
+
+    nodetool = createDefaultCustomTool(NAME)
+    nodetool.setInfo(
+        author="Liam Collod",
+        description="Merge xform transformations to the geometry.point.P attribute.",
+        version=VERSION,
+    )
+
+    configOpScript(nodetool.getDefaultOpScriptNode())
+    configTool(node=nodetool.node)
+
+    return nodetool.node
+
+
+if __name__ in ["__main__", "__builtin__", "Katana"]:
+    build()
