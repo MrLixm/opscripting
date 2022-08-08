1858a6c3c5d9ba0e1fd87760eab1b05c2e9357cd
Liam Collod <lcollod@gmail.com>
2022-08-08 22:48:29 +0200

    [dev] converted launcher paths to relative

--- dev/launcher.Katana-5.0.1.sh
+++ dev/launcher.Katana-5.0.1.sh
@@ -1,5 +1,7 @@
 # Katana launcher script
 
+cd ..
+
 KATANA_VERSION="5.0v1"
 KATANA_HOME="C:\Program Files\Katana$KATANA_VERSION"
 KATANA_TAGLINE="Opscripting DEV"
@@ -8,13 +10,13 @@ export PATH="$PATH;$KATANA_HOME\bin"
 export KATANA_CATALOG_RECT_UPDATE_BUFFER_SIZE=1
 
 export KATANA_USER_RESOURCE_DIRECTORY="Z:\dccs\katana\library\shelf0007\prefs"
-export KATANA_RESOURCES="$KATANA_RESOURCES;Z:\packages-dev\opscripting\dev\KatanaResources"
+export KATANA_RESOURCES="$KATANA_RESOURCES;.\dev\KatanaResources"
 
-export PYTHONPATH="$PYTHONPATH;Z:\packages-dev\typing"
-export PYTHONPATH="$PYTHONPATH;Z:\packages-dev\opscripting"
-export PYTHONPATH="$PYTHONPATH;Z:\packages-dev\katanaingcore"
+export PYTHONPATH="$PYTHONPATH;..\typing"
+export PYTHONPATH="$PYTHONPATH;..\opscripting"
+export PYTHONPATH="$PYTHONPATH;..\katanaingcore"
 
-export LUA_PATH="$LUA_PATH;Z:\packages-dev\opscripting\?.lua"
+export LUA_PATH="$LUA_PATH;.\?.lua"
 export LUA_PATH="$LUA_PATH;Z:\dccs\katana\library\shelf0006\lua\?.lua"
 
 "$KATANA_HOME\bin\katanaBin.exe"
