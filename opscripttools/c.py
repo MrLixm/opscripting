7fb9d5628d016fd8b85e464e75edca04a5a98d3d
Liam Collod <lcollod@gmail.com>
2022-08-10 23:21:09 +0200

    added more colors, added FLAVOR constant

--- opscripttools/c.py
+++ opscripttools/c.py
@@ -1,12 +1,27 @@
 class COLORS:
     """
     Colors for the entries in the LayeredMenu.
+    Can also be used to color the node.
+
+    Assumed to be sRGB - Display encoded.
     """
 
-    default = (0.39, 0.35, 0.466)  # Low contrast purple.
+    white = (0.75, 0.75, 0.75)
+    grey = (0.5, 0.5, 0.5)
+    black = (0.015, 0.015, 0.015)
+
+    green = (0.22, 0.46, 0.27)
+    bluelight = (0.27, 0.43, 0.46)
+    blue = (0.23, 0.27, 0.46)
+    purple = (0.39, 0.35, 0.466)
+    pink = (0.46, 0.27, 0.35)
+    red = (0.46, 0.16, 0.18)
+    yellow = (0.46, 0.41, 0.28)
+
+    default = purple
 
 
-SHORTCUT = "S"
+SHORTCUT = "O"
 """
 Shortcut to use in the Nodegraph to make the LayeredMenu appears.
 """
@@ -18,3 +33,5 @@ the LayeredMenu. Separator is the system path sperator (; or :):
 
 ex: ``"attr_math;xform2P;point_width"``
 """
+
+FLAVOR_NAME = "customTool"
