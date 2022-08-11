4af15d269d55731c2cc68ccf552cc09d990dbdf3
Liam Collod <lcollod@gmail.com>
2022-08-11 20:16:11 +0200

    removed AboutParam class, unified CustomToolNode class

--- opscripttools/tooling.py
+++ opscripttools/tooling.py
@@ -8,9 +8,8 @@ from typing import List
 from Katana import NodegraphAPI
 
 __all__ = (
-    "AboutParam",
-    "CustomTool",
-    "addAboutParamToNode",
+    "CustomToolNode",
+    "OpScriptTool",
 )
 
 logger = logging.getLogger(__name__)
@@ -21,6 +20,12 @@ VersionableType = Union[str, Union[List[int], Tuple[int, int, int]]]
 
 def versionize(version):
     # type: (VersionableType) -> str
+    """
+    Convert an object representing a potential version to a string representing
+    the version.
+
+    ex: (1,5,2) -> "1.5.2"
+    """
 
     if isinstance(version, str):
         return version
@@ -35,90 +40,35 @@ def versionize(version):
     )
 
 
-class AboutParam:
-    class Names:
+class CustomToolNode(NodegraphAPI.PythonGroupNode):
+    """
+    Abstract base class to create "CustomTool" nodes.
 
-        root = "about"
-        name = "name_"
-        version = "version_"
-        description = "info_"
-        author = "author_"
+    That's just a group node with some standards like an "about" param.
 
-    def __init__(self, node):
-        # type: (NodegraphAPI.Node) -> None
-
-        self.param = node.getParameter("user.{}".format(self.Names.root))
-
-        assert self.param, "user.{} doesn't exists on node {}".format(
-            self.Names.root, node
-        )
-
-        self.name = self.param.getChild(self.Names.name)
-        self.version = self.param.getChild(self.Names.version)
-        self.description = self.param.getChild(self.Names.description)
-        self.author = self.param.getChild(self.Names.author)
-
-        return
-
-    def moveToBottom(self):
-        """
-        Move the AboutParam parameter to the bottom of the `user` parameter layout.
-        """
-        parent = self.param.getParent()
-        parent.reorderChild(self.param, parent.getNumChildren() - 1)
-        return
-
-    def update(self, name="", version="", description="", author=""):
-        # type: (str, VersionableType, str, str) -> None
-        if name:
-            self.name.setValue(name, 0)
-        if version:
-            self.version.setValue(versionize(version), 0)
-        if description:
-            self.description.setValue(description, 0)
-        if author:
-            self.author.setValue(author, 0)
-        return
-
-    @classmethod
-    def createOn(cls, node, name="", version="0.1.0", description="", author=""):
-        # type: (NodegraphAPI.Node, str, VersionableType, str, str) -> AboutParam
-
-        usergrp = node.getParameter("user")
-        if not usergrp:
-            usergrp = node.getParameters().createChildGroup("user")
-            hint = {"hideTitle": True}
-            usergrp.setHintString(repr(hint))
-
-        assert not node.getParameter(
-            "user.{}".format(cls.Names.root)
-        ), "user.{} already exists on node {}".format(cls.Names.root, node)
-
-        aboutgrp = usergrp.createChildGroup(cls.Names.root)
-        aboutgrp.createChildString(cls.Names.name, name)
-        aboutgrp.createChildString(cls.Names.version, versionize(version))
-        aboutgrp.createChildString(cls.Names.description, description)
-        aboutgrp.createChildString(cls.Names.author, author)
-        return AboutParam(node)
-
-
-class CustomTool(NodegraphAPI.PythonGroupNode):
+    Its default structure correpond to one input and output port, and two dot nodes
+    connected together in the inside.
+    """
 
     port_in_name = "in"
     port_out_name = "out"
 
-    name = NotImplemented
-    color = None
-    version = NotImplemented
-    luamodule = None
+    name = NotImplemented  # type: str
+    version = NotImplemented  # type: Tuple[int,int,int]
+    color = None  # type: Tuple[float,float,float]
+    description = ""  # type: str
+    author = ""  # type: str
+    maintainers = []  # type: List[str]
 
     def __init__(self):
         super(CustomTool, self).__init__()
 
-        self.buildDefaultStructure()
+        self._about_param = None  # type: NodegraphAPI.Parameter
+        self.__buildAboutParam()
+        self.__buildDefaultStructure()
         self.__build()
 
-    def buildDefaultStructure(self):
+    def __buildDefaultStructure(self):
         """
         Create the basic nodegraph representation of a custom tool.
         This is a styled GroupNode with an OpScript node connected inside.
@@ -133,31 +83,21 @@ class CustomTool(NodegraphAPI.PythonGroupNode):
         # attr["ns_iconName"] = ""  # remove group icon
         # self.setAttributes(attr)
 
-        addAboutParamToNode(self, self.name)
-
-        node_opscript = NodegraphAPI.CreateNode("OpScript", self)
-        node_opscript.setName("OpScript_{}_0001".format(self.name))
-        node_opscript.getParameters().createChildGroup("user")
-
         pos = NodegraphAPI.GetNodePosition(self)
 
         node_dot_up = NodegraphAPI.CreateNode("Dot", self)
         NodegraphAPI.SetNodePosition(node_dot_up, (pos[0], pos[1] + 150))
-        node_dot_up.setName("Dot_{}_0001".format(self.name))
+        node_dot_up.setName("In_{}_0001".format(self.name))
 
         node_dot_down = NodegraphAPI.CreateNode("Dot", self)
         NodegraphAPI.SetNodePosition(node_dot_down, (pos[0], pos[1] - 150))
-        node_dot_down.setName("Dot_{}_0001".format(self.name))
+        node_dot_down.setName("Out_{}_0001".format(self.name))
 
         port_a = self.getSendPort(self.port_in_name)
         port_b = node_dot_up.getInputPortByIndex(0)
         port_a.connect(port_b)
 
         port_a = node_dot_up.getOutputPortByIndex(0)
-        port_b = node_opscript.getInputPortByIndex(0)
-        port_a.connect(port_b)
-
-        port_a = node_opscript.getOutputPortByIndex(0)
         port_b = node_dot_down.getInputPortByIndex(0)
         port_a.connect(port_b)
 
@@ -167,52 +107,70 @@ class CustomTool(NodegraphAPI.PythonGroupNode):
 
         return
 
+    def __buildAboutParam(self):
+
+        usergrp = self.getParameter("user")
+        if not usergrp:
+            usergrp = self.getParameters().createChildGroup("user")
+            hint = {"hideTitle": True}
+            usergrp.setHintString(repr(hint))
+
+        self._about_param = usergrp.createChildGroup(self.Names.root)
+        self._about_param.createChildString(self.Names.name, self.name)
+        self._about_param.createChildString(
+            self.Names.version, versionize(self.version)
+        )
+        self._about_param.createChildString(self.Names.description, self.description)
+        self._about_param.createChildString(self.Names.author, self.author)
+        return
+
     @abstractmethod
     def __build(self):
         pass
 
-    def getUserParam(self):
+    @property
+    def user_param(self):
         # type: () -> NodegraphAPI.Parameter
         return self.getParameter("user")
 
-    def getAboutParam(self):
-        # type: () -> AboutParam
-        return AboutParam(node=self)
-
-    def setVersion(self, version):
-        # type: (VersionableType) -> None
+    def moveAboutParamToBottom(self):
         """
-        Set the tool version on the ``user.about.version_`` parameter.
-
-        Args:
-            version: ex: "0.1.0"
+        Move the AboutParam parameter to the bottom of the `user` parameter layout.
         """
-        self.getAboutParam().update(version=version)
-
-    def getDefaultOpScriptNode(self):
-        # type: () -> Optional[NodegraphAPI.Node]
-        children = self.getChildren()
-        for child in children:
-            if child.getType() == "OpScript":
-                return child
+        self.user_param.reorderChild(
+            self._about_param,
+            self.user_param.getNumChildren() - 1,
+        )
         return
 
 
-def addAboutParamToNode(node, name="", version="0.1.0", description="", author=""):
-    # type: (NodegraphAPI.Node, str, VersionableType, str, str) -> AboutParam
+class OpScriptTool(CustomToolNode):
+
+    luamodule = NotImplemented
     """
-    Add the "about" group parameter to the given node.
-    This is added on custom tool to better track their origin.
-
-    This section contains 4 params::
-
-        ["name_", "version_", "info_", "author_"]
-
-    Args:
-        node: node to add the "user.about" parameter on.
-        name: name of the tool
-        version: version of the tool
-        description: short describtion about what the tool does
-        author: initial author of the tool
+    Every OpScript must live in a .lua registered in the LUA_PATH.
+    This means the OpScript.script will only import it using ``require()``
     """
-    return AboutParam.createOn(node, name, version, description, author)
+
+    def __buildDefaultStructure(self):
+
+        super(OpScriptTool, self).__buildDefaultStructure()
+
+        self._node_opscript = NodegraphAPI.CreateNode("OpScript", self)
+        self._node_opscript.setName("OpScript_{}_0001".format(self.name))
+        self._node_opscript.getParameters().createChildGroup("user")
+
+        NodegraphAPI.PackageSuperToolAPI.NodeUtils.WireInlineNodes(
+            parentGroupNode=self,
+            nodes=self.getChildren(),
+            x=150,
+        )
+        return
+
+    @abstractmethod
+    def __build(self):
+        pass
+
+    def getDefaultOpScriptNode(self):
+        # type: () -> Optional[NodegraphAPI.Node]
+        return self._node_opscript
