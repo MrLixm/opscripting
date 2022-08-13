7ad78b1c6cd18a32136dcd12386b0fc8b0e0c6df
Liam Collod <lcollod@gmail.com>
2022-08-13 13:45:56 +0200

    [lib] updated resolution_divide as OpScriptTool subclass

--- opscriptlibrary/resolution_divide.py
+++ opscriptlibrary/resolution_divide.py
@@ -2,27 +2,14 @@ import os.path
 
 from Katana import NodegraphAPI
 
-NAME = os.path.splitext(os.path.basename(__file__))[0]
-VERSION = (1, 0, 1)
+from customtooling.nodebase import OpScriptTool
 
 
-def build():
-    # type: () -> NodegraphAPI.Node
+def buildUserParams(userparam, as_expression=False):
+    # type: (NodegraphAPI.Parameter, bool) -> None
 
-    script = """
-local script = require("opscriptlibrary.{NAME}")
-script()"""
-    script = script.format(NAME=NAME)
-
-    node = NodegraphAPI.CreateNode("OpScript", NodegraphAPI.GetRootNode())
-    node.setName("OpScript_{}_1".format(NAME))
-
-    node.getParameter("applyWhere").setValue("at specific location", 0)
-    node.getParameter("script.lua").setValue(script, 0)
-    node.getParameter("location").setValue("/root", 0)
-
-    userparam = node.getParameters().createChildGroup("user")
     p = userparam.createChildNumber("divider", 1)
+    p.setExpression("=^/user.divider", as_expression)
     hint = {
         "help": (
             "<p>Amount to divide the current resolution by.<br>"
@@ -32,8 +19,44 @@ script()"""
     }
     p.setHintString(repr(hint))
 
-    return node
+    return
 
 
-if __name__ in ["__main__", "__builtin__", "Katana"]:
-    build()
+class ResolutionDivide(OpScriptTool):
+
+    name = "ResolutionDivide"
+    version = (1, 0, 0)
+    color = None
+    description = "Divide the current render resolution by the given amount."
+    author = "<Liam Collod pyco.liam.business@gmail.com>"
+    maintainers = []
+
+    luamodule = os.path.splitext(os.path.basename(__file__))[0]
+
+    def _buildOpScript(self):
+
+        script = """
+local script = require("opscriptlibrary.{module}")
+script()"""
+        script = script.format(module=self.luamodule)
+
+        node = self.getDefaultOpScriptNode()
+
+        node.getParameter("location").setValue("/root", 0)
+        node.getParameter("applyWhere").setValue("at specific location", 0)
+        node.getParameter("script.lua").setValue(script, 0)
+
+        buildUserParams(node.getParameter("user"), as_expression=True)
+        return
+
+    def _build(self):
+        # type: () -> NodegraphAPI.Node
+
+        self._buildOpScript()
+        buildUserParams(self.user_param, as_expression=False)
+
+        self.moveAboutParamToBottom()
+        return
+
+
+NODE = ResolutionDivide
