85fa9caad3ebb27316ae895f1edc935bdb3debe1
Liam Collod <lcollod@gmail.com>
2022-08-29 20:00:01 +0200

    [customtooling.c] added versioning constants

--- customtooling/c.py
+++ customtooling/c.py
@@ -1,6 +1,10 @@
 """
 Constants
 """
+__version_major__ = 0
+__version_minor__ = 1
+__version_patch__ = 0
+__version__ = "{}.{}.{}".format(__version_major__, __version_minor__, __version_patch__)
 
 
 class COLORS:
